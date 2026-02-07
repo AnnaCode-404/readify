1b87e7db80004704b642c5d430fe0323b906c874
AnnaCode-404 <wintersheran75@gmail.com>
2026-02-07 11:21:41 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "readify-cache-v1";
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./css/style.css",
+        "./js/main.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
